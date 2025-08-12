3bad1bbf304fdd35a8e089b35937918fbda16ce9
Douglas Jasper <doug.jasper@trios.com>
2025-08-12 08:38:22 -0400

    Added TodoCreator

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,24 +3,31 @@ import React, { useState } from 'react';
 import './App.css';
 import TodoBanner from './TodoBanner';
 import TodoRow from './TodoRow';
+import TodoCreator from './TodoCreator';
 
 function App() {
   const [userName] = useState("Doug");
 
-  const [todoItems] = useState([
+  const [todoItems, setTodoItems] = useState([
     { action: "Buy Flowers", done: false },
     { action: "Get Shoes", done: false },
     { action: "Collect Tickets", done: true },
     { action: "Call Joe", done: false }
   ]);
 
-  //const changeStateData = () => {
-  //  setUserName(prevName => (prevName === "Doug" ? "Bob" : "Doug"));
-  //};
+  const createNewTodo = (task) => {
+    if (!todoItems.find(item => item.action === task)) {
+      setTodoItems([...todoItems, { action: task, done: false }]);
+    }
+  };
 
   return (
     <div className="container mt-3">
       <TodoBanner userName={userName} todoItems={todoItems} />
+
+      <div className="m-3">
+        <TodoCreator callback={createNewTodo} />
+      </div>
 
       <table className="table table-striped table-bordered">
         <thead className="table-dark">
@@ -30,8 +37,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {todoItems.map((item, index) => (            
-            <TodoRow item={item} />
+          {todoItems.map((item, index) => (
+            <TodoRow key={index} item={item} />
           ))}
         </tbody>
       </table>
